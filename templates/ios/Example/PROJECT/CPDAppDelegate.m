be60ecc5a4b82b908911803a0dd368040550ff06
zhangjiong <DeveloperMichael@163.com>
2019-08-20 11:04:02 +0800

    no message

--- templates/ios/Example/PROJECT/CPDAppDelegate.m
+++ templates/ios/Example/PROJECT/CPDAppDelegate.m
@@ -26,10 +26,42 @@
     
     //设置 rootViewController
     self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
-    [[ZJAppConfiguration sharedInstance] zj_setupTarget:ZJTargetTypeWPUser environment:ZJEnvironmentTypeDevelopment];
-    [[ZJAppDelegateManager sharedInstance] trigger_applicationDidFinishLaunchingWithOptions:launchOptions];
+    [self setupCommonRootViewController];
+    
+    
+    
     return YES;
 }
 
+
+- (void)setupLoginRootViewController {
+    
+    [[ZJAppConfiguration sharedInstance] zj_setupTarget:ZJTargetTypeWPUser environment:ZJEnvironmentTypeDevelopment];
+    
+    if (![WPLoginManager wp_isUserLogin]) {
+        UIViewController <WPLoginServiceProtocol>*loginController = [ZJServiceManager createServiceWithProtocol:@protocol(WPLoginServiceProtocol)];
+        UINavigationController *nav;
+        if (loginController) {
+            nav = [[UINavigationController alloc] initWithRootViewController:loginController];
+            self.window.rootViewController = nav;
+        }
+        [self.window makeKeyAndVisible];
+    } else {
+        [[ZJAppConfiguration sharedInstance] zj_setUserToken:[WPLoginManager wp_token]];
+        [[ZJAppDelegateManager sharedInstance] trigger_applicationDidFinishLaunchingWithOptions:launchOptions];
+        [self.window makeKeyAndVisible];
+    }
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(appDidLoginSuccess) name:kWPDidUserLoginSuccessNotification object:nil];
+}
+
+- (void)setupCommonRootViewController {
+    //设置 rootViewController
+    self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
+    CPDViewController *vc = [[CPDViewController alloc] init];
+    UINavigationController *nav;
+    nav = [[UINavigationController alloc] initWithRootViewController:vc];
+    self.window.rootViewController = nav;
+}
 
 @end
